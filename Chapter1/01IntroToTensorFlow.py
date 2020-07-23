10562d30450f3c8853c51418264f865858a984c7
lalitgarg12 <lalitgarg12@gmail.com>
2020-07-23 05:53:29 +0530

    Minor Updates

--- Chapter1/01IntroToTensorFlow.py
+++ Chapter1/01IntroToTensorFlow.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 
 # Download and import the MIT 6.S191 package
 # !pip install mitdeeplearning
+# !pip install opencv-python
 import mitdeeplearning as mdl
 
 import numpy as np
@@ -13,3 +14,36 @@ number = tf.constant(1.41421356237, tf.float64)
 
 print("`sport` is a {}-d Tensor".format(tf.rank(sport).numpy()))
 print("`number` is a {}-d Tensor".format(tf.rank(number).numpy()))
+
+sports = tf.constant(["Tennis", "Basketball"], tf.string)
+numbers = tf.constant([3.141592, 1.414213, 2.71821], tf.float64)
+
+print("`sports` is a {}-d Tensor with shape: {}".format(tf.rank(sports).numpy(), tf.shape(sports)))
+print("`numbers` is a {}-d Tensor with shape: {}".format(tf.rank(numbers).numpy(), tf.shape(numbers)))
+
+### Defining higher-order Tensors ###
+
+'''TODO: Define a 2-d Tensor'''
+matrix = tf.constant([[1.0, 2.0, 3.0, 4.0], [5.0, 6.0, 7.0, 8.0]]) # TODO
+# matrix = # TODO
+
+assert isinstance(matrix, tf.Tensor), "matrix must be a tf Tensor object"
+assert tf.rank(matrix).numpy() == 2
+
+'''TODO: Define a 4-d Tensor.'''
+# Use tf.zeros to initialize a 4-d Tensor of zeros with size 10 x 256 x 256 x 3.
+#   You can think of this as 10 images where each image is RGB 256 x 256.
+images = tf.zeros([10, 256, 256, 3]) # TODO
+# images = # TODO
+
+assert isinstance(images, tf.Tensor), "matrix must be a tf Tensor object"
+assert tf.rank(images).numpy() == 4, "matrix must be of rank 4"
+assert tf.shape(images).numpy().tolist() == [10, 256, 256, 3], "matrix is incorrect shape"
+
+row_vector = matrix[1]
+column_vector = matrix[:,2]
+scalar = matrix[1, 2]
+
+print("`row_vector`: {}".format(row_vector.numpy()))
+print("`column_vector`: {}".format(column_vector.numpy()))
+print("`scalar`: {}".format(scalar.numpy()))
